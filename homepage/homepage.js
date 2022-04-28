6da2c26cdf98d7fd42fcc35a0922ec31bee5dcfe
Giang ca <91960480+Giang-st1-00@users.noreply.github.com>
2022-04-29 01:34:28 +0700

    card

--- homepage/homepage.js
+++ homepage/homepage.js
@@ -1,3 +1,4 @@
+//animate
 $('.ctn__content').addClass('animate__animated animate__bounceInUp');
 $('.ctn__image').addClass('animate__animated animate__bounceInRight');
 
@@ -11,11 +12,62 @@ $(document).ready(function(){
         } else if (body>=900 && body<1254) {
             $('.reason_title').addClass('animate__animated animate__bounceInUp');
             $('.info_item-1').addClass('animate__animated animate__bounceInUp');
-        } else if (body>=1254 && body<2000) {
+        } else if (body>=1254 && body<1866) {
             $('.info_item-2').addClass('animate__animated animate__bounceInUp');
-        
-        } 
+            
+        } else if (body>=1866 && body<3000) {
+            // $('.highlights .title').addClass('animate__animated animate__bounceInUp');
+            // $('.highlights .card_project').addClass('animate__animated animate__bounceInUp');
+        }
 
     });
 })
+
+//btn_project_card
+
+function trans_aft_card() {
+    let card_project = document.querySelector('.card_project');
+    let form_card_project = document.querySelectorAll('.card_project .form_card_project');
+    let card_project_1 = document.querySelector('.card_project-1');
+
+    Object.assign(card_project.style ,{
+        display : 'none',
+    })
+
+    Object.assign(card_project_1.style ,{
+        display : 'flex',
+    })
+}
+
+function trans_pre_card() {
+    let card_project = document.querySelector('.card_project');
+    let form_card_project = document.querySelectorAll('.card_project .form_card_project');
+    let card_project_1 = document.querySelector('.card_project-1');
+    
+    Object.assign(card_project_1.style ,{
+        display : 'none',
+    })
+    Object.assign(card_project.style ,{
+        display : 'flex',
+    })
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
