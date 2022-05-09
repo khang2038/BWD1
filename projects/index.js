dbd3e0fbe1d1a4eaa920a8230b52584a878ce8c7
Giang ca <91960480+Giang-st1-00@users.noreply.github.com>
2022-05-09 23:00:52 +0700

    anh

--- projects/index.js
+++ projects/index.js
@@ -14,3 +14,21 @@ const body=document.querySelector("body"),
               modeText.innerText="Dark Mode";
           }
       })
+
+
+// transform thanh chủ đề 
+function onmouseover_the_left_page() {
+    Object.assign(slidebar.style , {
+        animationName : 'trans_slide_bar_to_left',
+        animationDuration: '0.5s',
+        left: 0,
+    })
+}
+
+function onmouseout_the_left_page() {
+    Object.assign(slidebar.style , {
+        animationName : 'trans_slide_bar_to_right',
+        animationDuration: '0.5s',
+        left : '-14%',
+    })
+}
