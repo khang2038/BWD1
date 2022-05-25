29ab1df45dd7034bce935aa92aa3dc9ca3449743
khang2038 <trankhang9c2003@gmail.com>
2022-05-26 02:48:22 +0700

    dsads

--- NodeJS/src/app/controllers/productcontroller.js
+++ NodeJS/src/app/controllers/productcontroller.js
@@ -5,14 +5,16 @@ class productcontroller{
 
     index(req,res,next){
         product.find({})
-            .then(products=>res.render('product',{
+            .then(products=>{
+                products=products.map(product => product.toObject() )
+                res.render('product',{
                                         title: 'Product',
                                         style:'styleproduct.css',
                                         style2:'queriesproduct.css',
                                         script1:'scriptproduct.js',
                                         script2:'jquery.waypoints.min.js',
                                         products:products
-                                        })
+                                        })}
                     )
             .catch(next);
     }
