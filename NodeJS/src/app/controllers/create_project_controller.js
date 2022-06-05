39d3911fb9997d34ba662ac04f6ae57309b1bf38
Giang ca <91960480+Giang-st1-00@users.noreply.github.com>
2022-06-05 22:53:35 +0700

    hihihi

--- NodeJS/src/app/controllers/create_project_controller.js
+++ NodeJS/src/app/controllers/create_project_controller.js
@@ -2,8 +2,23 @@ const create_prj = require('../models/create_prj');
 const project_comment = require('../models/project_comment');
 const faqs = require('../models/question');
 const { mutipleMongooseToObject } = require('../../util/mongoose');
+const { mongooseToObject } = require('../../util/mongoose');
+
 var prj_cmt_db, project_db, faq_db;
 class create_project_controller{
+
+    show_detail(req, res, next) {
+        create_prj.findOne({slug : req.params.slug})
+            .then(project => {
+                res.render('create_project/create_project', {
+                    project : mongooseToObject(project),
+                    title : 'create_project',
+                    style : '../../css/create_project.css',
+                    script1 : '../../js/create_project.js',
+                });
+            })
+            .catch(next)
+    }
 
     index(req,res,next){
         project_comment.find({}) 
@@ -35,6 +50,8 @@ class create_project_controller{
             style : 'create_project.css',
         });
     }
+
+    
 }
 
 module.exports= new create_project_controller;
--- NodeJS/src/routes/create_project.js
+++ NodeJS/src/routes/create_project.js
@@ -3,6 +3,7 @@ const router=express.Router();
 
 const create_project_controller=require('../app/controllers/create_project_controller');
 
+router.get('/:slug',create_project_controller.show_detail);
 router.get('/',create_project_controller.index);
 
 module.exports = router;
