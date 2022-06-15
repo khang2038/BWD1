37072d21693e7a57e72d5daf68b7eea99866cf61
khang2038 <trankhang9c2003@gmail.com>
2022-06-16 00:30:00 +0700

    ádasd

--- NodeJS/src/public/js/profile.js
+++ NodeJS/src/public/js/profile.js
@@ -109,7 +109,17 @@ function trans_prj_favorite() {
         display : 'flex',
     })
 }
-
+function handle_user() {
+    var update1 = document.querySelector(`.user-drop`);
+    
+    if (update1.classList.contains('open1')){
+        update1.classList.remove('open1');
+    } 
+    else {
+        update1.classList.add('open1');
+    }
+    
+}
 
 
 
