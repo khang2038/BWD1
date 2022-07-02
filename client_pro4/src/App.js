e1b6811b2e6f8f4fb811689977ffd320426cde94
Giang ca <91960480+Giang-st1-00@users.noreply.github.com>
2022-07-03 03:42:53 +0700

    gg

--- client_pro4/src/App.js
+++ client_pro4/src/App.js
@@ -1,39 +1,38 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
 import { Container } from "@material-ui/core";
-import {Routes,Route} from 'react-router-dom';
-import Homepage from './pages/Homepage';
-import Cpn_header from '~/components/cpn_header/cpn_header.js';
-import Details from './pages/Details';
-import Home from './pages/Home';
-import Create_project from './pages/Create_project';
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Cpn_header from "~/components/cpn_header/cpn_header.js";
+import Details from "./pages/Details";
+import Home from "./pages/Home";
+import Create_project from "./pages/Create_project";
+import { useReducer } from "react";
+import AppReducer from "./Reducers/AppReducer";
+import AppContext from "./components/AppContext";
 
-
-import * as actions from './redux/actions';
-
-function App_logged() {
-  return (
-    <Container maxWidth={false} className="" style={{margin:'0', padding: '0'}}>
-      <Cpn_header />
-
-      <Routes>
-        <Route path="/homepage" element={<Homepage />}>
-
-        </Route>
-        <Route path="/details" element={<Details />}>
-
-        </Route>
-        <Route path="/create_project" element={<Create_project />}>
-
-        </Route>
-      </Routes>
-  </Container>
-  )
-}
+import * as actions from "./redux/actions";
 
 function App() {
+  const initialState = { user: null, posts: [] };
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
   return (
-    <App_logged />
-  )
+    <Container
+      maxWidth={false}
+      className=""
+      style={{ margin: "0", padding: "0" }}
+    >
+      <Cpn_header />
+
+      <AppContext.Provider>
+        <Routes>
+          <Route path="/homepage" element={<Homepage />}></Route>
+          <Route path="/details" element={<Details />}></Route>
+          <Route path="/create_project" element={<Create_project />}></Route>
+        </Routes>
+      </AppContext.Provider>
+    </Container>
+  );
 }
 
 export default App;
