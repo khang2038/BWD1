c31a77925db54be4ae7e18fd915a4a1038d5d396
Giang ca <91960480+Giang-st1-00@users.noreply.github.com>
2022-07-10 00:54:40 +0700

    animate

--- NodeJS/src/public/js/wallet.js
+++ NodeJS/src/public/js/wallet.js
@@ -1,9 +1,54 @@
+$('.ctn__solution__element').addClass('animate__animated animate__bounceInUp');
+const space_1 = document.querySelector('.space_1');
+
+function sleep(s){
+    return new Promise(function(resolve) {
+        setTimeout(resolve,s)
+    })
+}
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var body=$('html,body').scrollTop();
+        var width_screen = window.innerWidth;
+        console.log(width_screen);
+        console.log(body);
+        if (body>=300 && body<400 && width_screen>739) {
+            
+            
+        }
+        else if (body>400 && body<900) {
+            sleep(0)
+            .then(function() {
+
+                $('.ctn__feature__image').addClass('animate__animated animate__bounceInLeft');
+                $('.ctn__feature__content').addClass('animate__animated animate__bounceInRight');
+                
+                return sleep(500);
+            })
+            .then(function() {
+                Object.assign(space_1.style , {
+                    display: 'none',
+                })
+            
+            })     
+        }
+        else if (body>=1100 && body<1254 && width_screen>739) {
+            
+        } else if (body>=1254 && body<1866 && width_screen>739) {
+            
+        } else if (body>=1866 && body<3000 && width_screen>739) {
+            
+        }
+        
+    });
+})
+
 
 
 
 function handle_user() {
     var update1 = document.querySelector(`.user-drop`);
-    
     if (update1.classList.contains('open1')){
         update1.classList.remove('open1');
     } 
