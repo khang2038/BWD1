40478cc5a761628cabacd2f6063709390df5be1e
Giang ca <91960480+Giang-st1-00@users.noreply.github.com>
2022-07-11 04:31:36 +0700

    slider1

--- NodeJS/src/app/controllers/profile_controller.js
+++ NodeJS/src/app/controllers/profile_controller.js
@@ -1,13 +1,28 @@
 // const Project = require('../models/create_prj');
 // const Faqs = require('../models/question');
 // const Storys = require('../models/story');
+const { mutipleMongooseToObject } = require('../../util/mongoose');
+const { mongooseToObject } = require('../../util/mongoose');
 
+const create_prj = require('../models/create_prj');
+let create_prj_db;
 class post_project_created_controller{
     
-    index(req,res,next){
+    async index(req,res,next){
+        await create_prj.find()
+        .then(prj => {
+            prj = mutipleMongooseToObject(prj);
+            create_prj_db = prj.reverse();
+        })
+        .catch(next);
+
         res.render('profile/profile', {
             title : 'profile',
-            style : 'profile.css',
+            style : 'swiper-bundle.min.css',
+            style2 : 'profile.css',
+            script1 : 'swiper-bundle.min.js',
+            script3 : 'profile.js',
+            create_prj : create_prj_db,
             users:req.user
         });
     }
--- NodeJS/src/public/js/profile.js
+++ NodeJS/src/public/js/profile.js
@@ -353,3 +353,32 @@ function btn_hide_close() {
 
 
 
+// slider
+
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 2,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+    },
+  });
